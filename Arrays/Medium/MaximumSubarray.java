f8695f319760a977c7c500ecd419a403c3885422
Aryan Shakya <aryanshakya4549@gmail.com>
2026-01-05 15:47:25 +0530

    Day5 - MaximumSubarray Better Approach

--- Arrays/Medium/MaximumSubarray.java
+++ Arrays/Medium/MaximumSubarray.java
@@ -1,6 +1,21 @@
 // Problem: Find the subarray with the maximum sum and return its sum
 public class MaximumSubarray {
 
+// Better Approach
+// Time Complexity: O(n^2)
+// Space Complexity: O(1)
+    public static int better(int[] nums) {
+        int mSum = Integer.MIN_VALUE;
+        for( int i = 0 ; i < nums.length ; i++) {
+            int sum = 0;
+            for( int j = i ; j < nums.length ; j++) {
+                sum += nums[j];
+                mSum = Math.max(sum, mSum);
+            }
+        }
+        return mSum;
+    }
+
 // Brute Force
 // Time Complexity: O(n^3)
 // Space Complexity: O(1)
@@ -19,6 +34,6 @@ public class MaximumSubarray {
     }
     public static void main(String[] args) {
         int[] nums = { -2, -3, 4, -1, -2, 1, 5, -3};
-        System.out.println("Sum of maximum subarrays is: " + bruteForce(nums));
+        System.out.println("Sum of maximum subarrays is: " + better(nums));
     }
 }
