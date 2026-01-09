7441fe3a922b83d8938ee33f6090b6fb7c0b30f2
Aryan Shakya <aryanshakya4549@gmail.com>
2026-01-09 23:57:57 +0530

    Day9 - Print Name N Times

--- Basics/Recursion/PrintNTimes.java
+++ Basics/Recursion/PrintNTimes.java
@@ -13,7 +13,18 @@ public class PrintNTimes {
         printNTimes(string, n-1);
     }
 
+    public static void printNameNTimes(String name, int n) {
+
+        if (n <= 0) {
+            return;
+        }
+
+        System.out.println(name);
+        printNameNTimes(name, n-1);
+    }
+
     public static void main(String[] args) {
-        printNTimes("Hello", 10);
+        // printNTimes("Hello", 10);
+        printNameNTimes("Aryan Shakya", 10);
     }
 }
