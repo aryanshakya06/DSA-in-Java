9ca4661af19dec69427fed29fa13d2762c215445
Aryan Shakya <aryanshakya4549@gmail.com>
2026-01-15 21:24:33 +0530

    Day15 - LeetCode 42 TrappingRainWater

--- Arrays/Hard/TrappingRainWater.java
+++ Arrays/Hard/TrappingRainWater.java
@@ -4,9 +4,9 @@
 // Space Complexity: O(n)
 public class TrappingRainWater {
 
-    public static int trappingRainwater(int[] arr) {
+    public static int trappingRainwater(int[] height) {
 
-        int n = arr.length;
+        int n = height.length;
         if (n<=2) {
             return -1;
         }
@@ -15,26 +15,26 @@ public class TrappingRainWater {
         int[] leftMax = new int[n];
         int[] rightMax = new int[n];
 
-        leftMax[0] = arr[0];
+        leftMax[0] = height[0];
         for (int i = 1 ; i < n ; i++) {
-            leftMax[i] = Math.max(arr[i], leftMax[i-1]);
+            leftMax[i] = Math.max(height[i], leftMax[i-1]);
         }
 
-        rightMax[n-1] = arr[n-1];
+        rightMax[n-1] = height[n-1];
         for (int i = n-2 ; i >= 0 ; i--) {
-            rightMax[i] = Math.max(arr[i], rightMax[i+1]);
+            rightMax[i] = Math.max(height[i], rightMax[i+1]);
         }
 
         for (int i = 0 ; i < n ; i++) {
             int waterLevel = Math.min(leftMax[i], rightMax[i]);
-            trappedWater += waterLevel - arr[i];
+            trappedWater += waterLevel - height[i];
         }
 
         return trappedWater;
     }
 
     public static void main(String[] args) {
-        int[] arr = {4, 2, 0, 6, 3, 2 ,5};
-        System.out.println("Trapped Rainwater is: " + trappingRainwater(arr));                                                 
+        int[] height = {4, 2, 0, 6, 3, 2 ,5};
+        System.out.println("Trapped Rainwater is: " + trappingRainwater(height));                                                 
     }
 }                     
