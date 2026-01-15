ec1ed862999af6e8b2f7c194fa1cb7ebda75abce
Aryan Shakya <aryanshakya4549@gmail.com>
2026-01-15 21:39:50 +0530

    Day15 - String Palindrome

---
+++ Strings/Easy/StringPalindrome.java
@@ -0,0 +1,17 @@
+public class StringPalindrome {
+    
+    public static boolean isPalindrome(String s) {
+        int n = s.length();
+        for (int i = 0 ; i < n/2 ; i++) {
+            if (s.charAt(i) != s.charAt(n-i-1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = new String("racecar");
+        System.out.println(isPalindrome(s));
+    }
+}
