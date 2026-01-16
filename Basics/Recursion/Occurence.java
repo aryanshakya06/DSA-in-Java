29e964260fc1e68f356cabbd326296fd8693198c
Aryan Shakya <aryanshakya4549@gmail.com>
2026-01-16 12:22:59 +0530

    Day16 - Occurence in Array

---
+++ Basics/Recursion/Occurence.java
@@ -0,0 +1,47 @@
+public class Occurence {
+
+    public static int firstOccurence(int[] arr, int key, int i) {
+
+        if (i == arr.length) {
+            return -1;
+        }
+
+        if (arr[i] == key) {
+            return i;
+        }
+
+        return firstOccurence(arr, key, i+1);
+    }
+
+    public static int lastOccurence(int[] arr, int key, int i) {
+
+        if (i == arr.length) {
+            return -1;
+        }
+        
+        int isFound = lastOccurence(arr, key, i+1);
+        if (isFound == -1) {
+            if (arr[i] == key) {
+                return i;
+            }
+        }
+
+        return isFound;
+
+        // if (i < 0) {
+        //     return -1;
+        // }
+
+        // if (arr[i] == key) {
+        //     return i;
+        // }
+
+        // return lastOccurence(arr, key, i-1);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 8, 2, 5, 0 , 1, 5, 6};
+        System.out.println(firstOccurence(arr, 5, 0));
+        System.out.println(lastOccurence(arr, 5, 0));
+    }
+}
