062676f82e078723c1043f72b88050fef8893dde
Aryan Shakya <aryanshakya4549@gmail.com>
2026-01-20 22:16:01 +0530

    BitwiseOperators

---
+++ Basics/Bit Manipulation/BitwiseOperators.java
@@ -0,0 +1,12 @@
+// Bit Manipulation
+
+public class BitwiseOperators {
+    public static void main(String[] args) {
+        System.out.println(4 & 5);
+        System.out.println(4 | 5);
+        System.out.println(4 ^ 5);
+        System.out.println(~0);
+        System.out.println(~1);
+        System.out.println(~5);
+    }
+}
