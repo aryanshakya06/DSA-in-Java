cffcdf41e4cfb192b228c2693deefbd9a78dfdfb
Aryan Shakya <aryanshakya4549@gmail.com>
2026-01-30 23:19:51 +0530

    Day30 - LeetCode 73 Better Approach

--- Arrays/Medium/SetMatrixZeroes.java
+++ Arrays/Medium/SetMatrixZeroes.java
@@ -2,8 +2,9 @@
 // LeetCode 73
 public class SetMatrixZeroes {
 
-    // Time Complexity: O(m * n * (m + n))
-    // Space Complexity: O(1)
+    // Better Approach
+    // Time Complexity: O(m * n)
+    // Space Complexity: O(m + n)
     public static void setZeroes(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
@@ -12,53 +13,76 @@ public class SetMatrixZeroes {
             return;
         }
 
+        int[] row = new int[m];
+        int[] col = new int[n];
+
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (matrix[i][j] == 0) {
-                    markRow(i, matrix);
-                    markCol(j, matrix);
+                    row[i] = 1;
+                    col[j] = 1;
                 }
             }
         }
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == -1) {
+        for (int i = 0 ; i < m ; i++) {
+            for (int j = 0 ; j < n ; j++) {
+                if (row[i] == 1 || col[j] == 1) {
                     matrix[i][j] = 0;
                 }
             }
         }
     }
 
-    public static void markRow(int i, int[][] matrix) {
-        int n = matrix[0].length;
-        int j = 0;
-        while (j < n) {
-            if (matrix[i][j] != 0) {
-                matrix[i][j] = -1;
-            }
-            j++;
-        }
-    }
+    // Time Complexity: O(m * n * (m + n))
+    // Space Complexity: O(1)
+    // public static void setZeroes(int[][] matrix) {
+    //     int m = matrix.length;
+    //     int n = matrix[0].length;
 
-    public static void markCol(int j, int[][] matrix) {
-        int m = matrix.length;
-        int i = 0;
-        while (i < m) {
-            if (matrix[i][j] != 0) {
-                matrix[i][j] = -1;
-            }
-            i++;
-        }
-    }
+    //     if (m < 1 || n < 1) {
+    //         return;
+    //     }
 
-    public static void printArray(String arrayName, int[] arr) {
-        System.out.print(arrayName + ": ");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-    }
+    //     for (int i = 0; i < m; i++) {
+    //         for (int j = 0; j < n; j++) {
+    //             if (matrix[i][j] == 0) {
+    //                 markRow(i, matrix);
+    //                 markCol(j, matrix);
+    //             }
+    //         }
+    //     }
+
+    //     for (int i = 0; i < m; i++) {
+    //         for (int j = 0; j < n; j++) {
+    //             if (matrix[i][j] == -1) {
+    //                 matrix[i][j] = 0;
+    //             }
+    //         }
+    //     }
+    // }
+
+    // public static void markRow(int i, int[][] matrix) {
+    //     int n = matrix[0].length;
+    //     int j = 0;
+    //     while (j < n) {
+    //         if (matrix[i][j] != 0) {
+    //             matrix[i][j] = -1;
+    //         }
+    //         j++;
+    //     }
+    // }
+
+    // public static void markCol(int j, int[][] matrix) {
+    //     int m = matrix.length;
+    //     int i = 0;
+    //     while (i < m) {
+    //         if (matrix[i][j] != 0) {
+    //             matrix[i][j] = -1;
+    //         }
+    //         i++;
+    //     }
+    // }
 
     public static void printMatrix(String name, int[][] matrix) {
         int rows = matrix.length;
