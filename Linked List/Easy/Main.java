1c5123b50256905d03bf6d835a7bf0491ab6c2f7
Aryan Shakya <aryanshakya4549@gmail.com>
2026-01-31 16:10:26 +0530

    Day31 - OOP correction in linkedlist implementation

---
+++ Linked List/Easy/Main.java
@@ -0,0 +1,335 @@
+public class Main {
+
+    public static class LinkedList {
+
+        public class Node {
+            int data;
+            Node next;
+
+            public Node(int data) {
+                this.data = data;
+                this.next = null;
+            }
+        }
+
+        public Node head;
+        public Node tail;
+        public int size;
+
+        // Time Complexity: O(1)
+        // Space Complexity: O(1)
+        public void addFirst(int data) {
+            Node newNode = new Node(data);
+            size++;
+
+            if (head == null) {
+                head = tail = newNode;
+                return;
+            }
+
+            newNode.next = head;
+            head = newNode;
+        }
+
+        // Time Complexity: O(1)
+        // Space Complexity: O(1)
+        public void addLast(int data) {
+            Node newNode = new Node(data);
+            size++;
+
+            if (head == null) {
+                head = tail = newNode;
+                return;
+            }
+
+            tail.next = newNode;
+            tail = newNode;
+        }
+
+        // Time Complexity: O(n) -> traversal up to index
+        // Space Complexity: O(1)
+        public void add(int index, int data) {
+
+            if (index < 0 || index > size) {
+                System.out.println("Invalid Index");
+                return;
+            }
+
+            if (index == 0) {
+                addFirst(data);
+                return;
+            }
+
+            if (index == size) {
+                addLast(data);
+                return;
+            }
+
+            Node newNode = new Node(data);
+            size++;
+            int i = 0;
+            Node temp = head;
+            while (i != index - 1) {
+                if (temp == null) {
+                    break;
+                }
+                i++;
+                temp = temp.next;
+            }
+
+            newNode.next = temp.next;
+            temp.next = newNode;
+        }
+
+        public void add(int data) {
+            addLast(data);
+        }
+
+        // Time Complexity: O(1)
+        // Space Complexity: O(1)
+        public int removeFirst() {
+            if (size == 0) {
+                System.out.println("Empty LinkedList");
+                return -1;
+            }
+            int data = head.data;
+
+            if (head == tail) {
+                head = tail = null;
+            } else {
+                head = head.next;
+            }
+
+            size--;
+
+            return data;
+        }
+
+        // Time Complexity: O(n) -> traversal to second last node
+        // Space Complexity: O(1)
+        public int removeLast() {
+            if (size == 0) {
+                System.out.println("Empty LinkedList");
+                return -1;
+            }
+
+            int data = tail.data;
+
+            if (head == tail) {
+                head = tail = null;
+            } else {
+                Node previous = head;
+                while ((previous.next).next != null) {
+                    previous = previous.next;
+                }
+
+                previous.next = null;
+                tail = previous;
+            }
+
+            size--;
+
+            return data;
+        }
+
+        // Time Complexity: O(n)
+        // Space Complexity: O(1)
+        public int iterativeSearch(int key) {
+            Node temp = head;
+            int i = -1;
+
+            while (temp != null) {
+                i++;
+                if (temp.data == key) {
+                    return i;
+                }
+
+                temp = temp.next;
+            }
+
+            return -1;
+        }
+
+        // Time Complexity: O(n)
+        // Space Complexity: O(n) -> call stack
+
+        public int helper(Node head, int key) {
+            if (head == null) {
+                return -1;
+            }
+
+            if (head.data == key) {
+                return 0;
+            }
+
+            int index = helper(head.next, key);
+            if (index == -1) {
+                return -1;
+            }
+
+            return index + 1;
+        }
+
+        // Time Complexity: O(n)
+        // Space Complexity: O(n)
+        public int recursiveSearch(int key) {
+            return helper(head, key);
+        }
+
+        // Time Complexity: O(n)
+        // Space Complexity: O(1)
+        public void print() {
+            Node temp = head;
+            while (temp != null) {
+                System.out.print(temp.data + "->");
+                temp = temp.next;
+            }
+            System.out.println("null");
+        }
+
+        // Time Complexity: O(n)
+        // Space Complexity: O(1)
+        public void reverse() {
+            Node prev = null;
+            Node curr = tail = head;
+            Node next;
+
+            while (curr != null) {
+                next = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = next;
+            }
+
+            head = prev;
+        }
+
+        // Time Complexity: O(n)
+        // Space Complexity: O(1)
+        public void removeNthFromEnd(int n) {
+            if (head == null) {
+                System.out.println("Empty LinkedList");
+                return;
+            }
+
+            int s = 0;
+            Node temp = head;
+
+            while (temp != null) {
+                temp = temp.next;
+                s++;
+            }
+
+            if (n <= 0 || n > s) {
+                System.out.println("Invalid index");
+                return;
+            }
+
+            if (n == s) {
+                head = head.next;
+                if (head == null) {
+                    tail = null;
+                }
+                size--;
+                return;
+            }
+
+            int i = 1;
+            Node prev = head;
+
+            while (i < s - n) {
+                prev = prev.next;
+                i++;
+            }
+
+            // if tail
+            if (prev.next == tail) {
+                tail = prev;
+            }
+
+            prev.next = prev.next.next;
+            size--;
+        }
+
+        // slow-fast approach
+        public Node findMid(Node head) {
+            if (head == null) {
+                return head;
+            }
+
+            Node slow = head;
+            Node fast = head;
+
+            while (fast != null && fast.next != null) {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+            return slow;
+        }
+
+        public boolean isPalindrome() {
+            if (head == null || head.next == null) {
+                return true;
+            }
+
+            Node mid = findMid(head);
+
+            Node prev = null;
+            Node curr = mid;
+            Node next;
+
+            while (curr != null) {
+                next = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = next;
+            }
+
+            Node rightHead = prev;
+            Node leftHead = head;
+
+            while (rightHead != null) {
+                if (leftHead.data != rightHead.data) {
+                    return false;
+                }
+
+                leftHead = leftHead.next;
+                rightHead = rightHead.next;
+            }
+
+            return true;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.addLast(1);
+        list.addLast(2);
+        list.addLast(2);
+        list.addLast(1);
+        list.print();
+        System.out.println(list.isPalindrome());
+
+        // LinkedList list = new LinkedList();
+        // list.addFirst(20);
+        // list.addFirst(10);
+        // list.addLast(40);
+        // list.addLast(50);
+        // list.add(2, 30);
+        // list.print();
+        // list.RemoveNthFromEnd(3);
+        // list.print();
+
+        // System.out.println(list.removeFirst());
+        // System.out.println(list.removeLast());
+        // list.reverse();
+        // list.print();
+        // System.out.println(list.iterativeSearch(40));
+        // System.out.println(list.iterativeSearch(100));
+        // System.out.println(list.recursiveSearch(40));
+        // System.out.println(list.recursiveSearch(100));
+    }
+}
