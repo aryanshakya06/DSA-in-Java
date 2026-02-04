1f50e8f36626a484b1081389dbc7d265e9cc4fdc
Aryan Shakya <aryanshakya4549@gmail.com>
2026-02-04 23:46:28 +0530

    Day35 - LeetCode 142 Detect Cycle

---
+++ Linked List/Medium/DetectCycle.java
@@ -0,0 +1,48 @@
+public class DetectCycle {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+
+        }
+    }
+
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        boolean cycle = false;
+        
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                cycle = true;
+                break;
+            }
+        }
+
+        if (!cycle) {
+            return null;
+        }
+
+        slow = head;
+
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+}
