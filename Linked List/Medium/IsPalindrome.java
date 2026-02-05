4a3cd79da55b75070e5c645003986b7afc642ad8
Aryan Shakya <aryanshakya4549@gmail.com>
2026-02-05 20:42:16 +0530

    Day36 - LeetCode 234 LL isPalindrome

---
+++ Linked List/Medium/IsPalindrome.java
@@ -0,0 +1,74 @@
+// Problem: Check the linked list is palindrome or not
+// LeetCode 234
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+public class IsPalindrome {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+
+        }
+    }
+
+    public ListNode findMid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode curr = head;
+        ListNode prev = null;
+        ListNode next;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    public boolean isPalindrome(ListNode head) {
+
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        ListNode mid = findMid(head);
+
+        ListNode left = head;
+        ListNode right = reverseList(mid.next);
+        mid.next = null;
+
+        while (left != null && right != null) {
+            if (left.val != right.val) {
+                return false;
+            }
+            left = left.next;
+            right = right.next;
+        }
+
+        return true;
+    }
+}
