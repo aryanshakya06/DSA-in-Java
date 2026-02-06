ac752b42c9b2c9a9e4967d76961308633e4d9c7f
Aryan Shakya <aryanshakya4549@gmail.com>
2026-02-06 23:53:59 +0530

    Day37 - LeetCode 2095 Delete middle

---
+++ Linked List/Medium/DeleteMiddle.java
@@ -0,0 +1,43 @@
+// Problem: Delete the middle node of a singly linked list and return the head
+// LeetCode 2095
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+public class DeleteMiddle {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+
+        }
+    }
+
+    public ListNode deleteMiddle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        prev.next = slow.next;
+
+        return head;
+    }
+}
