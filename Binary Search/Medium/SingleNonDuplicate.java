e73f55ca32da79fd2e6b07a1c39605d343dbcaff
Aryan Shakya <aryanshakya4549@gmail.com>
2026-02-09 23:45:48 +0530

    Day40 - LeetCode 540 Single Non Duplicate

---
+++ Binary Search/Medium/SingleNonDuplicate.java
@@ -0,0 +1,42 @@
+// Problem: Find the single element in a sorted array where every other element appears exactly twice
+// LeetCode 540
+// Time Complexity: O(log n)
+// Space Complexity: O(1)
+public class SingleNonDuplicate {
+    class Solution {
+        public int singleNonDuplicate(int[] nums) {
+            int n = nums.length;
+
+            if (n == 1) {
+                return nums[0];
+            }
+
+            if (nums[0] != nums[1]) {
+                return nums[0];
+            }
+
+            if (nums[n - 1] != nums[n - 2]) {
+                return nums[n - 1];
+            }
+
+            int low = 1;
+            int high = n - 2;
+
+            while (low <= high) {
+                int mid = (low + high) / 2;
+
+                if (nums[mid] != nums[mid + 1] && nums[mid] != nums[mid - 1]) {
+                    return nums[mid];
+                }
+
+                if ((mid % 2 == 1 && nums[mid] == nums[mid - 1]) || (mid % 2 == 0 && nums[mid] == nums[mid + 1])) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
