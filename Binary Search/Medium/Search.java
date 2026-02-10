29710ff06b86598fa9d1a936066aabf14d84137c
Aryan Shakya <aryanshakya4549@gmail.com>
2026-02-10 23:31:03 +0530

    Day41 - LeetCode 81

---
+++ Binary Search/Medium/Search.java
@@ -0,0 +1,40 @@
+// Problem: Search in Rotated Sorted Array II (array may contain duplicates)
+// LeetCode 81
+// Time Complexity: O(log n) 
+// Space Complexity: O(1)
+public class Search {
+    public boolean search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                return true;
+            }
+
+            if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
+                low++;
+                high--;
+                continue;
+            }
+
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        return false;
+    }
+}
