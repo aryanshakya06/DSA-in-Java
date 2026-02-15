51229a29970f17b1d1e3a23a6573be5317bd9804
Aryan Shakya <aryanshakya4549@gmail.com>
2026-02-15 23:52:34 +0530

    Day46 - LeetCode 83 Remove Duplicates

---
+++ Linked List/Easy/RemoveDuplicates.java
@@ -0,0 +1,40 @@
+// Problem: Remove duplicates from a sorted linked list and return the head of the updated list
+// LeetCode 83
+// Time: O(n) – traverse the list once
+// Space: O(1) – in-place modification, no extra space used
+
+public class RemoveDuplicates {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null)
+            return null;
+
+        ListNode current = head;
+
+        while (current != null && current.next != null) {
+            if (current.val == current.next.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+
+        return head;
+    }
+}
