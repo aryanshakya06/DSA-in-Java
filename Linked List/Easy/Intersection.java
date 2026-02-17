9dc714c65dbf94da7cb1d0ee54438bef12a6201d
Aryan Shakya <aryanshakya4549@gmail.com>
2026-02-17 23:08:01 +0530

    Day48 LeetCode 160 Intersection of LL

---
+++ Linked List/Easy/Intersection.java
@@ -0,0 +1,37 @@
+// Problem: Find the intersection node of two singly linked lists, if it exists.
+// LeetCode 160 
+// Time Complexity: O(m + n)
+// Space Complexity: O(1)
+public class Intersection {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null)
+            return null;
+
+        ListNode a = headA;
+        ListNode b = headB;
+
+        while (a != b) {
+            a = (a == null) ? headB : a.next;
+            b = (b == null) ? headA : b.next;
+        }
+
+        return a;
+    }
+}
