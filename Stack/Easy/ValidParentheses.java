54646403a50987abadba3e6256ed12bbbc06cb4d
Aryan Shakya <aryanshakya4549@gmail.com>
2026-02-24 23:51:40 +0530

    Day55 LeetCode 20 Valid Parentheses

---
+++ Stack/Easy/ValidParentheses.java
@@ -0,0 +1,31 @@
+// Problem: LeetCode 20 - Valid Parentheses
+// Approach: Stack
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+import java.util.Stack;
+
+class ValidParentheses {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty())
+                    return false;
+
+                char top = stack.pop();
+
+                if ((ch == ')' && top != '(') ||
+                        (ch == '}' && top != '{') ||
+                        (ch == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
