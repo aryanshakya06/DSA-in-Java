a795ece8ece585c4bed8c63b385b97254714c0a9
Aryan Shakya <aryanshakya4549@gmail.com>
2026-02-26 23:38:56 +0530

    Day57 LeetCode 496 NGE 1

---
+++ Stack/Easy/NGE1.java
@@ -0,0 +1,32 @@
+// Problem: Next Greater Element I
+// LeetCode 496
+// Time Complexity: O(n + m)
+// Space Complexity: O(n)
+
+import java.util.*;
+
+class NGE1 {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int num : nums2) {
+            while (!stack.isEmpty() && stack.peek() < num) {
+                map.put(stack.pop(), num);
+            }
+            stack.push(num);
+        }
+        
+        while (!stack.isEmpty()) {
+            map.put(stack.pop(), -1);
+        }
+        
+        int[] result = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            result[i] = map.get(nums1[i]);
+        }
+        
+        return result;
+    }
+}
